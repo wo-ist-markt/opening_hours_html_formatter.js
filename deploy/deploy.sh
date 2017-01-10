9a28ed38e4d53cc6ab08396abaa12e16f99de430
Tobias Preuss <tobias.preuss@googlemail.com>
2017-01-10 19:35:48 +0100

    Truncate line break at the tail of the date.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,8 +4,8 @@
 # Prepend version information
 
 now="$(date +'%Y-%m-%d %r')"
+now="$(echo $now | sed -e 's/[\r\n]//g')"
 out=dist/opening_hours_html_formatter.js
-
 
 cat lib/.version > $out
 echo "// Generated at $now" >> $out
